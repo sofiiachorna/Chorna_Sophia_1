a086273365b811ce12f734de08d8a4b44038baef
sofiiachorna <sonya.chorna@gmail.com>
2022-06-02 09:31:38 +0300

    second commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker worker = new Worker("James", "Skavo", 4000, 56);
+            worker.Calculate();
+
+            Console.WriteLine(worker.Serialize());
+
+            Worker worker2 = worker.Deserialize("{\"Name\":\"Bob\",\"Surname\":\"Dilan\",\"Salary\":123000,\"Days\":12}");
+            worker2.Calculate();
+        }
+    }
+}
---
+++ Worker.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Lab_1
+{
+    class Worker
+    {
+        public string Name;
+        public string Surname;
+        public int Salary;
+        public int Days;
+
+        public Worker() { }
+
+        public Worker(string n, string s, int m, int d)
+        {
+            Name = n;
+            Surname = s;
+            Salary = m;
+            Days = d;
+
+        }
+        public void Calculate()
+        {
+            Console.WriteLine(this.Name + " " + this.Surname + " ");
+            Console.WriteLine("salary: " + this.Salary);
+            Console.WriteLine("worked for " + this.Days + " days");
+            Console.WriteLine("sum=" + Salary * Days + " $");
+        }
+        public string Serialize()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+        public Worker Deserialize(string json)
+        {
+            Worker worker = JsonConvert.DeserializeObject<Worker>(json);
+            return worker;
+        }
+    }
+}
